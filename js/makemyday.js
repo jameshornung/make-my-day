104c99ebf3a5ea385fd5b0d6159e8e0a05c7b240
chanceeakin <chance.eakin@gmail.com>
2016-06-11 22:06:38 -0500

    added bower component for OAuth

--- js/makemyday.js
+++ js/makemyday.js
@@ -1,24 +1,23 @@
-
 ////////////// VARIABLE DECLARATION
 var Choices = {
-    dateNights: {
-        dateOne: {
-            restaurant: {
-                name: 'Salty Sow',
-                address: '1917+manor+road,austin,tx',
-                labelLetter: 'S',
-                markerColor: 'blue'
-            },
-            bar: {
-                name: 'Butterfly Bar',
-                address: '2307+Manor+Rd,Austin,TX+78722',
-                labelLetter: 'B',
-                markerColor: 'red'
+        dateNights: {
+            dateOne: {
+                restaurant: {
+                    name: 'Salty Sow',
+                    address: '1917+manor+road,austin,tx',
+                    labelLetter: 'S',
+                    markerColor: 'blue'
+                },
+                bar: {
+                    name: 'Butterfly Bar',
+                    address: '2307+Manor+Rd,Austin,TX+78722',
+                    labelLetter: 'B',
+                    markerColor: 'red'
+                }
             }
         }
     }
-}
-////// THIS WILL CHANGE WITH SOME ///////////////
+    ////// THIS WILL CHANGE WITH SOME ///////////////
 var date = Choices.dateNights.dateOne;
 
 
@@ -26,8 +25,10 @@ var date = Choices.dateNights.dateOne;
 
 
 
-    $(document).ready(function() {
+$(document).ready(function() {
     $('#make-my-day').on('click', function() {
-        $('#googlemap').html("<img src='https://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=roadmap&markers=color:" + date.restaurant.markerColor + "%7Clabel:" + date.restaurant.labelLetter + "%7C" + date.restaurant.address + "&markers=color:" + date.bar.markerColor + "%7Clabel:"+ date.bar.labelLetter + "%7C" + date.bar.address + "&key=AIzaSyAO1ybpbxzx7RMHZHcwK_-S48cRjMmWJcI' alt='map'>");
+        $('#googlemap').html("<img src='https://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=roadmap&markers=color:" + date.restaurant.markerColor + "%7Clabel:" + date.restaurant.labelLetter + "%7C" + date.restaurant.address + "&markers=color:" + date.bar.markerColor + "%7Clabel:" + date.bar.labelLetter + "%7C" + date.bar.address + "&key=AIzaSyAO1ybpbxzx7RMHZHcwK_-S48cRjMmWJcI' alt='map'>");
     })
+
+
 })
