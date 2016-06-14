fd9452d59ff6008b604c12c490b9edbce6db3e8e
chanceeakin <chance.eakin@gmail.com>
2016-06-13 22:15:44 -0500

    chance additions, mostly useless:

--- js/makemyday.js
+++ js/makemyday.js
@@ -2,6 +2,7 @@
 var Choices = {
         dateNights: {
             dateOne: {
+                parking: {},
                 restaurant: {
                     name: 'Salty Sow',
                     address: '1917+manor+road,austin,tx',
@@ -25,10 +26,10 @@ var date = Choices.dateNights.dateOne;
 
 
 
+
+
 $(document).ready(function() {
     $('#make-my-day').on('click', function() {
         $('#googlemap').html("<img src='https://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=roadmap&markers=color:" + date.restaurant.markerColor + "%7Clabel:" + date.restaurant.labelLetter + "%7C" + date.restaurant.address + "&markers=color:" + date.bar.markerColor + "%7Clabel:" + date.bar.labelLetter + "%7C" + date.bar.address + "&key=AIzaSyAO1ybpbxzx7RMHZHcwK_-S48cRjMmWJcI' alt='map'>");
     })
-
-
 })
