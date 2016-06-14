d6b052b7780aea9edba6dafb9327295533475a94
Chance Eakin <chance.eakin@gmail.com>
2016-06-14 16:01:42 -0500

    google places addition

---
+++ js/EakinPseudocode.md
@@ -0,0 +1,15 @@
+Alright. Here's what we need to do:
+
+Get Google Places Details data.
+
+Put it into the Places Div.
+
+
+Get Instagram Data.
+
+Put it in the Instagram Div.
+
+Issues:
+How do I get the data from Google?
+Can I use places with a static map?
+
--- js/makemyday.js
+++ js/makemyday.js
@@ -7,13 +7,15 @@ var Choices = {
                     name: 'Salty Sow',
                     address: '1917+manor+road,austin,tx',
                     labelLetter: 'S',
-                    markerColor: 'blue'
+                    markerColor: 'blue',
+                    placeID: 'ChIJ61i-aZO1RIYRKgxgoJHJc281917'
                 },
                 bar: {
                     name: 'Butterfly Bar',
                     address: '2307+Manor+Rd,Austin,TX+78722',
                     labelLetter: 'B',
-                    markerColor: 'red'
+                    markerColor: 'red',
+                    placeID: 'ChIJ_SqzjOy1RIYR09ZBlUuVbyc'
                 }
             }
         }
@@ -24,12 +26,24 @@ var date = Choices.dateNights.dateOne;
 
 /////////add a custom icon to labelLetter! /////////////
 
-
+$.ajax({
+    url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + date.bar.placeID + '&key=AIzaSyC1HEKxkiAsyrFzLSt_Mm39q0_lR8U4UgU',
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp'
+})
+.done(function(response) {
+    console.log("success")
+    console.log(response);
+})
+.fail(function() {
+    console.log("error");
+});
 
 
 
 $(document).ready(function() {
     $('#make-my-day').on('click', function() {
-        $('#googlemap').html("<img src='https://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=roadmap&markers=color:" + date.restaurant.markerColor + "%7Clabel:" + date.restaurant.labelLetter + "%7C" + date.restaurant.address + "&markers=color:" + date.bar.markerColor + "%7Clabel:" + date.bar.labelLetter + "%7C" + date.bar.address + "&key=AIzaSyAO1ybpbxzx7RMHZHcwK_-S48cRjMmWJcI' alt='map'>");
+        $('#googlemap').html("<img src='https://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=roadmap&markers=color:" + date.restaurant.markerColor + "%7Clabel:" + date.restaurant.labelLetter + "%7C" + date.restaurant.address + "&markers=color:" + date.bar.markerColor + "%7Clabel:" + date.bar.labelLetter + "%7C" + date.bar.address + "&key=AIzaSyC1HEKxkiAsyrFzLSt_Mm39q0_lR8U4UgU' alt='map'>");
     })
 })
